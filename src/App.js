d581edbfbcbef051c410d1fae68b7707da7c95f3
Arnold Gandarillas Castillo <arkgast@gmail.com>
2018-10-06 10:00:10 -0400

    feat: simple student register app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  state = {
+    student: {
+      name: '',
+      lastName: ''
+    },
+    studentList: []
+  }
+  handleChange = e => {
+    const { name, value } = e.target
+    const { student } = this.state
+    this.setState({
+      student: {
+        ...student,
+        [name]: value
+      }
+    })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    const { student, studentList } = this.state
+    this.setState({
+      studentList: [ student, ...studentList ],
+      student: {
+        name: '',
+        lastName: ''
+      }
+    })
+  }
   render() {
+    const { student, studentList } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='container'>
+        <div className='form-wrapper'>
+          <h1>Student info</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type='text'
+              name='name'
+              placeholder='Name'
+              value={student.name}
+              onChange={this.handleChange}
+            />
+            <input
+              type='text'
+              name='lastName'
+              placeholder='Last Name'
+              value={student.lastName}
+              onChange={this.handleChange}
+            />
+            <button>Save</button>
+          </form>
+        </div>
+        <div className='list-wrapper'>
+          <h1>Student list</h1>
+          <ul>
+            {studentList.map((student, key) => (
+              <li key={key}>{student.name} {student.lastName}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default App
